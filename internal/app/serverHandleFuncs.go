fef1aba73a0599e8db5184e5c09b09a9de68e7d8
Andronov Dmitriy <42334635+Andronovdima@users.noreply.github.com>
2019-10-26 13:01:47 +0300

    test

--- internal/app/serverHandleFuncs.go
+++ internal/app/serverHandleFuncs.go
@@ -17,6 +17,7 @@ import (
 )
 
 func (s *server) HandleCreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", s.clientUrl)
 
